97c64521e25a549d9f1d6a5bff6c6f26c8f3eebb
Kirti Chhabra <125173248+Kirtichhabra@users.noreply.github.com>
2023-06-06 22:18:39 +0530

    Add files via upload

---
+++ service/CredService.java
@@ -0,0 +1,8 @@
+package com.greatlearning.service;
+import com.greatlearning.model.*;
+
+public interface CredService {
+	String generateEmailId(String firstname, String lastname,String dept);
+	String generatePassword();
+	void showDetails(Employee e1);
+}
